0b82f0eb90939429f70fffef0bf0556ad47791a7
Vali-R <ruziboev.vali@sun-asterisk.com>
2022-10-27 03:48:09 -0400

    [BE][Laravel] Send a confirmation email after signing up && Create API to login users

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/signup', [AuthController::class, 'signup']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
